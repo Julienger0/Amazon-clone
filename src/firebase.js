e3e57c62b474493431c879572ed44860a73d4989
Julien <gerard.julien2000@orange.fr>
2022-03-24 15:38:45 +0100

    HomePage

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHTOx80rETDkxJ_ZQp5b9sqYHC6OGYJUg",
+  authDomain: "c-2c907.firebaseapp.com",
+  projectId: "c-2c907",
+  storageBucket: "c-2c907.appspot.com",
+  messagingSenderId: "396939549818",
+  appId: "1:396939549818:web:deb5748f4f09c151362fc3",
+};
