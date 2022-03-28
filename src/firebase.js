52c9f60cdd88b87c4724a13f42e9cbda54857547
Julien <gerard.julien2000@orange.fr>
2022-03-28 16:11:56 +0200

    Payment Page

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBHTOx80rETDkxJ_ZQp5b9sqYHC6OGYJUg",
   authDomain: "c-2c907.firebaseapp.com",
@@ -6,3 +10,8 @@ const firebaseConfig = {
   messagingSenderId: "396939549818",
   appId: "1:396939549818:web:deb5748f4f09c151362fc3",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+export { db, auth };
